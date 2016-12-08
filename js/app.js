fdfa4cc35b3bd6b62d2a07b992bde1e793e7bab4
collinl34 <collinlshelby@gmail.com>
2016-12-08 09:36:29 -0800

    reviews added to product nested object and new div with repeat directive for all reviews add

--- js/app.js
+++ js/app.js
@@ -1,46 +1,52 @@
 (function() {
-  // declaring a angular module
+    // declaring a angular module
     var app = angular.module('store', []);
 
     // create controller for store module
     app.controller('StoreController', function() {
-        this.products = gems;
+        this.products = [
+        {
+            name: 'gem stone',
+            price: 2.86,
+            description: 'this is a way better description of this gem then what was there before',
+            forSale: true,
+            soldOut: false,
+            images: [{
+                full: 'images/gem.gif'
+            }],
+            reviews: [{
+                stars: 5,
+                body: 'i love this gem sooo much',
+                author: 'collin@mgial.com'
+            }]
+        }, {
+            name: 'gem stone',
+            price: 100,
+            description: 'this is my favorite gem it is the most amazing omg i love this gem',
+            forSale: true,
+            soldOut: false,
+            images: [{
+                full: 'images/gem2.gif'
+            }],
+            reviews: [{
+                stars: 3,
+                body: 'i love this gem too',
+                author: 'collin@mgial.com'
+            }]
+        }];
     });
 
-    // declaring gems nested objects within array
-    var gems = [{
-        name: 'gem stone',
-        price: 2.86,
-        description: 'this is a way better description of this gem thne what was there before',
-        forSale: true,
-        soldOut: false,
-        images: [
-        {
-          full: 'gem.gif'
-        }]
-    }, {
-        name: 'gem stone',
-        price: 100,
-        description: 'this is my favorite gem it is the most amazing omg i love this gem',
-        forSale: true,
-        soldOut: false,
-        images: [
-        {
-          full: 'gem2.gif'
-        }]
-    }];
-
     // creating 
-    app.controller('PanelController',function() {
-      this.tab = 1;
-      // panelcontroller method
-      this.selectTab = function(setTab) {
-        this.tab = setTab;
-      };
-      // another panelcontroller method
-      this.isSelected = function(selectTab) {
-        return this.tab === selectTab;
-      };
+    app.controller('PanelController', function() {
+        this.tab = 1;
+        // panelcontroller method
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        // another panelcontroller method
+        this.isSelected = function(selectTab) {
+            return this.tab === selectTab;
+        };
     });
 
 })();
