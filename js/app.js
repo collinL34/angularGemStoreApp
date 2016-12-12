9886f49dc1ee0cdb7d5f1327d75692caac50f25e
collinl34 <collinlshelby@gmail.com>
2016-12-11 17:34:24 -0800

    api json file create

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 (function() {
     // declaring a angular module
-    var app = angular.module('store', ['store-products']);
+    var app = angular.module('store', ['$http','store-products']);
 
     // create controller for store module
-    app.controller('StoreController', ['$http', function($http) {
+    app.controller('StoreController', ['$http',function($http) {
         var store = this;
 
         store.products = [];
 
-        $http.get('products.json').success( function(data) {
+        $http.get('/products.json').success( function(data) {
             store.products = data;
+            console.log(data);
         });
     }]);
 })();
